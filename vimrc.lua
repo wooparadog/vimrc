67eae15fd2e775549ac268bf0d9d7c941600cb81
WooParadog <guohaochuan@gmail.com>
2022-05-08 23:27:52 +0800

    Remove placeholders

--- vimrc.lua
+++ vimrc.lua
@@ -1,6 +1,3 @@
-require'lspconfig'.pyright.setup{}
-require'lspconfig'.gopls.setup{}
-
 -- Setup nvim-cmp.
 local cmp = require'cmp'
 
@@ -65,7 +62,6 @@ cmp.setup.cmdline(':', {
 
 -- Setup lspconfig.
 local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
--- Replace <YOUR_LSP_SERVER> with each lsp server you've enabled.
-require('lspconfig')['<YOUR_LSP_SERVER>'].setup {
-  capabilities = capabilities
-}
+
+require'lspconfig'.pyright.setup{}
+require'lspconfig'.gopls.setup{}
