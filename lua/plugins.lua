0e15fc133c1f16fc2fd24daceef56ec582253c52
WooParadog <guohaochuan@gmail.com>
2022-06-01 20:56:29 +0800

    Add clang lsp

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -4,6 +4,7 @@ require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabil
 require('lspconfig').pyright.setup{}
 require('lspconfig').gopls.setup{}
 require('lspconfig').tsserver.setup{}
+require('lspconfig').clangd.setup{}
 
 -- Others
 require('gitsigns').setup()
