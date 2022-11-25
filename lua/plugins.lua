5e39dfa2ab3e2e3709d331fac243e72be2cabbb3
WooParadog <guohaochuan@gmail.com>
2022-11-25 22:09:07 +0800

    f

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,6 +1,6 @@
 #! /usr/bin/env lua
 
-require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
+require('cmp_nvim_lsp').default_capabilities(vim.lsp.protocol.make_client_capabilities())
 require('lspconfig').pyright.setup{}
 require('lspconfig').gopls.setup{}
 require('lspconfig').tsserver.setup{}
@@ -9,12 +9,12 @@ require('lspconfig').clangd.setup{}
 -- Others
 require('gitsigns').setup()
 require("bufferline").setup{}
-require("nvim-gps").setup()
 
 require('statusline')
 require('complete')
 require('browser')
 require('treesitter')
+require("nvim-gps").setup()
 
 require('diff')
 require('saga')
